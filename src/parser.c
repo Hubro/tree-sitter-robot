b761372af09e6568930ceec2412472179f8751af
Tomas Sandven <tomas@sandven.email>
2026-03-03 17:05:57 +0100

    Bump version to 1.3.0

--- src/parser.c
+++ src/parser.c
@@ -11173,7 +11173,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_robot(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 1,
-      .minor_version = 2,
+      .minor_version = 3,
       .patch_version = 0,
     },
   };
